1fdae772a23cb67c17b09c1821317dd9486a1cc8
Maciej Derda <maciejderda@wp.pl>
2023-12-11 19:02:59 +0100

    optimise
    -code optimization: task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,11 @@ const ingredients = [
   "Condiments",
 ];
 const list = document.getElementById("ingredients");
+const listArray = [];
 for (const ingredient of ingredients) {
   const listItem = document.createElement("li");
   listItem.textContent = ingredient;
   listItem.classList.add("item");
-  list.append(listItem);
+  listArray.push(listItem);
 }
+list.append(...listArray);
